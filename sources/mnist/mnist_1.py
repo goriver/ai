8f7b3d056cba08fbce721ac06b7b4f77dbde653f
Kenneth, Lee <edowon0623@gmail.com>
2020-08-27 16:30:45 +0900

    upload

--- sources/mnist/mnist_1.py
+++ sources/mnist/mnist_1.py
@@ -1,13 +1,59 @@
 import numpy
-import matploylib as plt
+import matplotlib as plt
+import scipy.special
 
 class NeuralNetwork:
     # 초기화
     def __init__(self, inputnodes, hiddennodes, outputnodes, learnrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # 가중치 행렬 wih, who 
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes)) 
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        self.lr = learnrate
+
+        # 활성화 함수 지정 
+        #self.activation_func = lambda x : scipy.special.expit(x)
+        # activation_func(5)
+        # sigmoid(5)
         pass
 
+    def sigmoid(self, x):
+        return 1 / (1 + numpy.exp(-x))
+    
     # 학습
     def train(self, inputs_list, targets_list):
+        # 1.입력 리스트를 2차원 행렬로 변환
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # 2. 은닉계층으로 들어오는 값 계산 (input -> hidden) 
+        hidden_inputs = numpy.dot(self.wih, inputs)
+
+        # 3. 은닉계층에서 나가는 값 계산 (hidden -> output) -> sigmoid
+        hidden_outputs = self.sigmoid(hidden_inputs)
+
+        # 4. 최종계층으로 들어오는 값 계산 (hidden -> output)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+
+        # 5. 최종계층에서 나가는 값 계산 
+        final_outputs = self.sigmoid(final_inputs)
+
+        # 6. 오차 계산 (실제 값 - 모델에 의해서 계산 된 값)
+        output_errors = targets - final_outputs
+
+        # 7. 오차역전법 (은닉계층의 오차는 가중치에 의해 나뉜 출력 계층의 오차들의 재조합)
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # 8. 은닉계층과 출력계층 간 가중치 업데이트
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1- final_outputs)), numpy.transpose(hidden_outputs))
+
+        # 9. 입력계층과 은닉계층 간 가중치 업데이트 
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1- hidden_outputs)), numpy.transpose(inputs))
+
         pass
 
     # 검증(테스트)
